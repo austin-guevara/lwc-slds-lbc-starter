eae5e652fca4c38dccf8ea03424104c27ad36812
Austin Guevara <a.guevara+sfemu@salesforce.com>
2026-03-02 14:15:19 -0500

    Add lightning-combobox for test

--- src/modules/main/app/app.js
+++ src/modules/main/app/app.js
@@ -1,4 +1,25 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorldApp extends LightningElement {
+  selectedCountry = '';
+
+  countryOptions = [
+    { label: 'United States', value: 'US' },
+    { label: 'Canada', value: 'CA' },
+    { label: 'United Kingdom', value: 'GB' },
+    { label: 'Germany', value: 'DE' },
+    { label: 'France', value: 'FR' },
+    { label: 'Spain', value: 'ES' },
+    { label: 'Italy', value: 'IT' },
+    { label: 'Australia', value: 'AU' },
+    { label: 'Japan', value: 'JP' },
+    { label: 'Mexico', value: 'MX' },
+    { label: 'Brazil', value: 'BR' },
+    { label: 'India', value: 'IN' },
+    { label: 'China', value: 'CN' },
+  ];
+
+  handleCountryChange(event) {
+    this.selectedCountry = event.detail.value;
+  }
 }
